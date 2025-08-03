fa65b3dc2810e9724f0c5db9e574a3ccdf9ccb01
Dimple Baruah <dimplebaruah@gmail.com>
2025-08-03 20:46:21 +0530

    Update app.py with correct port binding for Render

--- app.py
+++ app.py
@@ -1,21 +1,13 @@
-from flask import Flask, render_template
-from datetime import datetime
+from flask import Flask, render_template, url_for
+
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    today = datetime.now()
-    birthday = datetime(2025, 8, 8, 12, 0, 0)  # 8 August, 12 PM
-    days_left = (birthday - today).days
-    hours_left = (birthday - today).seconds // 3600
-    show_locked = today < birthday
+def home():
+    return render_template('index.html')
 
-    return render_template('index.html', 
-                           days_left=days_left,
-                           hours_left=hours_left,
-                           show_locked=show_locked,
-                           today_date=today)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
